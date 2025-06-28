7c3d167a6ae85ccdf46805f502a0f9478e42f4e6
Andre Aguirre <89417334+Nightmare33n@users.noreply.github.com>
2025-06-27 22:38:56 -0600

    arreglando pagina

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-window.addEventListener('load', () => {
-  const tabActivo = document.querySelector('.mode-tab.active');
-  if (tabActivo) {
-    const modo = tabActivo.dataset.mode;
-    const contenidoMostrar = document.getElementById(modo);
-
-    // Mostrar el contenido con animación inicial
-    if (contenidoMostrar) {
-      contenidoMostrar.style.display = 'block';
-      contenidoMostrar.classList.add('cuadro-animar');
-
-      // Quitar animación después de reproducirse
-      contenidoMostrar.addEventListener('animationend', () => {
-        contenidoMostrar.classList.remove('cuadro-animar');
-      }, { once: true });
-    }
-  }
-});
+ 
